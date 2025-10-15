0b1f0c1ca1f129d8192bd0db45f5bcb8cdcbd1f2
ashhyren <ashhyren@hotmail.com>
2025-10-14 18:27:10 -0700

    Update 10/14 18.27

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -5,19 +5,30 @@ from textnode import TextNode, TextType
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD)
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.TEXT)
         self.assertEqual(node, node2)
 
-    def test_n_eq(self):
-        node = ("This is a text node", TextType.ITALIC, "http://www.boot.dev")
-        node2 = ("This is a text node", TextType.LINK, "http://www.boot.dev")
+    def test_eq_false(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
-    def test_eq1(self):
-        node=TextNode("Test",TextType.IMAGE)
-        node2=TextNode("Test",TextType.IMAGE, None)
-        self.assertEqual(node,node2)
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node2", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
 
 
 if __name__ == "__main__":
